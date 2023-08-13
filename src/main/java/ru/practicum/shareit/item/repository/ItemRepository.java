acecbcf813d89b27953e50e478ec978c3317469a
Aleksandr <alkor1603@yandex.ru>
2023-08-13 22:56:23 +0300

    ТЗ 15 Сделал отступ

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -16,5 +16,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             " or upper(i.description) like upper(concat('%', ?1, '%'))" +
             " and i.available = true")
     List<Item> getItemsByTextOfQuery(String textOfQuery, Pageable pageable);
+
     List<Item> getItemsByItemRequestId(Long requestId);
 }
